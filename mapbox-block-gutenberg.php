d748f31f92b45f71d6a6b32b5980b00f1205384b
Sam Hermes <samhermes@me.com>
2018-10-02 22:20:02 -0500

    Add settings page link to plugin actions

--- mapbox-block-gutenberg.php
+++ mapbox-block-gutenberg.php
@@ -21,3 +21,14 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Block Initializer.
  */
 require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
+
+/**
+ * Add settings page link to plugin actions.
+ */
+function mapbox_block_add_settings_link( $links ) {
+	$settings_link = '<a href="options-general.php?page=mapbox-block-settings">' . __( 'Settings' ) . '</a>';
+	array_push( $links, $settings_link );
+	
+	return $links;
+}
+add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'mapbox_block_add_settings_link' );
